dd249f52d9dcfb13718e362af857e8a755712f95
18810720286 <2985219028@qq.com>
2020-04-10 21:44:10 +0800

    冲突已经解决

--- SpringProject/src/org/test/dao/IStudentDao.java
+++ SpringProject/src/org/test/dao/IStudentDao.java
@@ -1,11 +1,7 @@
 package org.test.dao;
 
 import org.test.entity.Student;
-<<<<<<< HEAD
-//123abc456
-=======
-//123abc123
->>>>>>> branch 'master' of git@github.com:18810720286/eclipsegitremote.git
+//123abc456+123abc123
 public interface IStudentDao {
 	public void addStudent(Student student);
 }
